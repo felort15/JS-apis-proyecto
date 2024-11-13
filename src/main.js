26ed14351d771ffdf7b2592e87007601ce2fd338
felort1512 <felort15@gmail.com>
2024-11-12 22:45:27 -0600

    Comentario de las funciones

--- src/main.js
+++ src/main.js
@@ -1,31 +1,40 @@
-async function getTrendingMoviesPrevew() {
-    const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
-const data = await res.json();
-const movies = data.results;
-movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector
-('#trendingPreview .trendingPreview-movieList');
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+  },
+});
 
-    const movieContainer = document.createElement('div')
-    movieContainer.classList.add("movie-container");
+async function getTrendingMoviesPreview() {
+  const { data } = await api('trending/movie/day');
+  const movies = data.results;
+
+  movies.forEach(movie => {
+    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
 
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
-    movieImg.setAttribute('alt', movie.title); // Add alt attribute
-    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path,
     );
+
     movieContainer.appendChild(movieImg);
     trendingPreviewMoviesContainer.appendChild(movieContainer);
-});
+  });
 }
-getTrendingMoviesPrevew()
-
 
 async function getCategegoriesPreview() {
-  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-  const data = await res.json();
-
+  const { data } = await api('genre/movie/list');
   const categories = data.genres;
+
   categories.forEach(category => {
     const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
     
@@ -44,4 +53,4 @@ async function getCategegoriesPreview() {
 }
 
 //getTrendingMoviesPreview();
-getCategegoriesPreview();
+//getCategegoriesPreview();
